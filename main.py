419040999cfc97b0fca47efac739e93b10d19c2e
ZhuravlevaM <66254978+ZhuravlevaM@users.noreply.github.com>
2024-02-20 09:41:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ from telebot import types
 bot = telebot.TeleBot('6656400001:AAHPefzdqUZkWcCFm18Q7ZpudoBoUEpCfBU')
 
 main_keyboard = types.ReplyKeyboardMarkup()
-main_keyboard.row('Тренировка', 'Добавить слово', "Удалить слово", "FAQ")
+main_keyboard.row('Тренировка', 'Добавить слово', "Удалить слово")
+main_keyboard.row('Словарь', "FAQ")
+dictionary = {}
 
 @bot.message_handler(commands=['start'])
 def welcome(message):
@@ -14,13 +16,32 @@ def welcome(message):
 def send_message(message):
     text = message.text.lower()
     if text == 'тренировка':
-        bot.send_message(message.from_user.id,'в разработке')
+        bot.send_message(message.from_user.id,'сейчас мы начнем тренировать слова, имеющиеся в вашем словаре')
     elif text == 'добавить слово':
-        bot.send_message(message.from_user.id, 'в разработке')
+        bot.send_message(message.from_user.id, 'введите слово и его перевод в формате: "link ссылка"')
+        bot.register_next_step_handler(message, get_pair_words)
     elif text == 'удалить слово':
-        bot.send_message(message.from_user.id, 'в разработке')
+        bot.send_message(message.from_user.id, 'слово удалено из вашего славоря')
+    elif text == "словарь":
+        bot.send_message(message.from_user.id, 'полный словарь ниже')
+        show_dictionary(message)
     else:
         bot.send_message(message.from_user.id, 'Я вас не понял')
 
-bot.polling(none_stop=True)
 
+def get_pair_words(message):
+    pair = message.text.split()
+    if len(pair) == 2:
+        dictionary[pair[0]] = pair[1]
+        bot.send_message(message.from_user.id, 'Слово добавлено')
+    else:
+        bot.send_message(message.from_user.id, 'Формат ввода должен быть как в примере:"link ссылка"')
+
+def show_dictionary(message):
+    all_dict_for_print = ''
+    for key, value in dictionary:
+        all_dict_for_print += key + ' ' + value + '\n'
+    bot.send_message(message.from_user.id, all_dict_for_print)
+    bot.send_message(message.from_user.id,'Всего слов в словаре: ' + str(len(all_dict_for_print)))
+
+bot.polling(none_stop=True)
