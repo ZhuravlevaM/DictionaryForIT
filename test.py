ff450c5c36378d5d29d180c128e49a98282a481c
ZhuravlevaM <zhuravleva1990@gmail.com>
2025-02-12 15:48:48 +0400

    copy from dictionaryForIt_bot2

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 import json
-#def load_from_json(data): #обработка json и вывод словаря
-with open('test_data.json', 'r+', encoding='utf-8') as file:
-    data = file.read().replace(u"\xa0", " ")# Load JSON as a list of dictionaries
-    print(data)
-    file.write(data)
-char = ' '
-print(f"Символ: {repr(char)}, Код: {ord(char)}")
+def load_from_json(): #обработка json и вывод словаря
+    with open('test_data.json', 'r', encoding='utf-8') as file:
+        data = file.read().replace(' ', " ")# Load JSON as a list of dictionaries
+    with open('test_data.json', 'w', encoding='utf-8') as file:
+        file.write(data)
+#char = ' '
+#print(f"Символ: {repr(char)}, Код: {ord(char)}")
 
+
+load_from_json()
