ecd542410107e3f9532a7535ca8ceb4e1f4cf1d0
ZhuravlevaM <zhuravleva1990@gmail.com>
2025-02-27 23:12:52 +0400

    add #comment     27.02.25

--- replace_NBSP_in_dict.py
+++ replace_NBSP_in_dict.py
@@ -2,7 +2,7 @@ import json
 from pair_words import PairWords
 def load_from_json(): #обработка json и вывод словаря
     with open('data.json', 'r', encoding='utf-8') as file:
-        data = file.read().replace(' ', " ")# Load JSON as a list of dictionaries
+        data = file.read().replace(' ', " ")# Load JSON as a list of dictionaries замена символа на пробел
     with open('data.json', 'w', encoding='utf-8') as file:
         file.write(data)
 char = ' '
